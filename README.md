26cae7f3ca0f465b5935e81061a4864c11672492
sharan <shazan.singh@gmail.com>
2022-08-20 21:30:06 +1000

    Uni_Landing-page

---
+++ README.md
@@ -0,0 +1,7 @@
+# OVERVIEW
+
+University Landing page website using HTML5, CSS3 and Vanilla JS
+
+# Demo
+
+https://poetic-longma-1d203d.netlify.app/
---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('hi')
+
+let navLinks = document.getElementById('navLinks');
+
+
+console.log(navLinks)
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+    // navLinks.style.display = 
+}
